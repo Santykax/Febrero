a8da6c37a56cc3dc471e3d1265adb1cc6b48b807
Santykax <soysantiagokaiser@gmail.com>
2018-02-28 09:07:26 -0300

    Cree los models con los primeros atributos

--- fc_kaiser/facturas/admin.py
+++ fc_kaiser/facturas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from facturas.models import *
+
+admin.site.register(Factura)
+admin.site.register(Item)
--- fc_kaiser/facturas/models.py
+++ fc_kaiser/facturas/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Factura(models.Model):
+    nombre = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=1)
+    tipob = models.BooleanField()
+    
+
+class Item(models.Model):
+    producto = models.CharField(max_length=50)
+    precio = models.FloatField()
+    cantidad = models.IntegerField()
